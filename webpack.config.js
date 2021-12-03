e558c4819158a6f13b31d7bbb9e020fcc342a3ba
neuroticks <neuroticks@gmail.com>
2021-12-03 08:44:57 -0300

    Webpack configurado para nao realizar refresh da pagina, mas ainda atualizar o conteudo dos arquivos no servidor

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const postCSSPlugins = [
-  require('postcss-import'),
+  require("postcss-import"),
   require("postcss-simple-vars"),
   require("postcss-nested"),
   require("autoprefixer"),
@@ -13,23 +13,26 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "app"),
   },
+  devServer: {
+    before: function (app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
   mode: "development",
-  watch: true,
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [
           "style-loader",
-          "css-loader",
+          "css-loader?url=false",
           {
             loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                url: false,
-                plugins: postCSSPlugins,
-              },
-            },
+            options: { postcssOptions: { plugins: postCSSPlugins } },
           },
         ],
       },
