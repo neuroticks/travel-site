1feb770c2f572095db204d8bacf4e3c4e753abf0
neuroticks <neuroticks@gmail.com>
2021-12-16 18:30:18 -0300

    Mostrar elementos somente quando seçao torna-se a area visivel na tela

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,37 +1,54 @@
-import { throttle } from "lodash/throttle";
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
 
 class RevealOnScroll {
-  constructor() {
-    this.itemsToReveal = document.querySelectorAll(".feature-item");
-    this.hideInitially();
-    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
-    this.events();
+  constructor(els, thresholdPercent) {
+    this.thresholdPercent = thresholdPercent
+    this.itemsToReveal = els
+    this.browserHeight = window.innerHeight
+    this.hideInitially()
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+    this.events()
   }
 
   events() {
-    window.addEventListener("scroll", this.scrollThrottle);
+    window.addEventListener("scroll", this.scrollThrottle)
+    window.addEventListener("resize", debounce(() => {
+      console.log("Resize just ran")
+      this.browserHeight = window.innerHeight
+    }, 333))
   }
 
   calcCaller() {
-    console.log("Scroll function called");
-    this.itemsToReveal.forEach((el) => {
-      this.calculateIfScrolledTo(el);
-    });
+    console.log("Scroll function ran")
+    this.itemsToReveal.forEach(el => {
+      if (el.isRevealed == false) {
+        this.calculateIfScrolledTo(el)
+      }
+    })
   }
 
   calculateIfScrolledTo(el) {
-    console.log("Elemento foi calculado");
-    let scrollPercent =
-      (el.getBoundingClientRect().y / window.innerHeight) * 100;
-
-    if (scrollPercent < 75) {
-      el.classList.add("reveal-item--is-visible");
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      console.log("Element was calculated")
+      let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100
+      if (scrollPercent < this.thresholdPercent) {
+        el.classList.add("reveal-item--is-visible")
+        el.isRevealed = true
+        if (el.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle)
+        }
+      }
     }
   }
 
   hideInitially() {
-    this.itemsToReveal.forEach((el) => el.classList.add("reveal-item"));
+    this.itemsToReveal.forEach(el => {
+      el.classList.add("reveal-item")
+      el.isRevealed = false
+    })
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
   }
 }
 
-export default RevealOnScroll;
+export default RevealOnScroll
