a3881be5065feaff4314a9272027c2b60ff41dbb
neuroticks <neuroticks@gmail.com>
2021-12-16 17:56:50 -0300

    Commitado com erro só pra manter histórico do fato

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,19 +1,26 @@
+import { throttle } from "lodash/throttle";
+
 class RevealOnScroll {
   constructor() {
     this.itemsToReveal = document.querySelectorAll(".feature-item");
     this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
   events() {
-    window.addEventListener("scroll", () => {
-      this.itemsToReveal.forEach((el) => {
-        this.calculateIfScrolledTo(el);
-      });
+    window.addEventListener("scroll", this.scrollThrottle);
+  }
+
+  calcCaller() {
+    console.log("Scroll function called");
+    this.itemsToReveal.forEach((el) => {
+      this.calculateIfScrolledTo(el);
     });
   }
 
   calculateIfScrolledTo(el) {
+    console.log("Elemento foi calculado");
     let scrollPercent =
       (el.getBoundingClientRect().y / window.innerHeight) * 100;
 
